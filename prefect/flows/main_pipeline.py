c28221918a27d2fd7b27263bb5c25ea148c298c5
anggerenn <anggernolnem@gmail.com>
2026-02-19 18:57:43 +0700

    add  evidence_rebuild.py

--- prefect/flows/main_pipeline.py
+++ prefect/flows/main_pipeline.py
@@ -2,11 +2,13 @@ import path_setup
 from prefect import flow
 from dlt_ingestion import run_dlt
 from dbt_transformation import run_dbt
+from evidence_rebuild import rebuild_evidence
 
 @flow(name="analytics_pipeline")
 def analytics_pipeline():
     run_dlt()
     run_dbt()
+    rebuild_evidence()
 
 if __name__ == "__main__":
     analytics_pipeline()
